e23d896b57305ef827e68724ed20f61c5eb09814
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-04-28 14:00:07 +0200

    mission model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_115119) do
+ActiveRecord::Schema.define(version: 2020_04_28_115350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "missions", force: :cascade do |t|
+    t.string "company"
+    t.string "location"
+    t.string "favorite"
+    t.string "start_date"
+    t.string "end_date"
+    t.string "status"
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_missions_on_user_id"
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
@@ -36,5 +50,6 @@ ActiveRecord::Schema.define(version: 2020_04_28_115119) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "missions", "users"
   add_foreign_key "notes", "users"
 end
