b9f2e532d0453769aa0152b5118bd9b680737584
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-04-28 18:16:55 +0200

    notes controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,51 @@
+class NotesController < ApplicationController
+ def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    @note.user = current_user
+    # raise
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    @note.update(note_params)
+    if @note
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+   @note = Note.find(params[:id])
+   @note.destroy
+   redirect_to notes_path
+  end
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
+
+end
